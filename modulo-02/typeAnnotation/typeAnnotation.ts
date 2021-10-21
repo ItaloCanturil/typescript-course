4fe31a44f5e1ea8f3eb0f5912a703b7b5a7d15ef
Italo Canturil <italo.cantur@gmail.com>
2021-10-20 22:56:42 -0300

    type annotation

---
+++ modulo-02/typeAnnotation/typeAnnotation.ts
@@ -0,0 +1,20 @@
+// Variaveis
+let nome: string = 'Italo'
+console.log(nome)
+// Arrays
+let animals: string[] = ['dog', 'cat', 'coruja']
+console.log(animals)
+// Objects
+let carro: {
+  modelo: string,
+  ano: number,
+  preco: number
+};
+
+carro = { modelo: 'Tesla', ano: 2021, preco: 100000}
+// Functions
+function multiplicaNumeros(num1: number, num2: number) {
+  return num1 * num2;
+}
+
+console.log(multiplicaNumeros(3,1))
