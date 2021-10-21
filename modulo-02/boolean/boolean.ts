a0b610ec8038a8fa54581ebf86e7521f961734aa
Italo Canturil <italo.cantur@gmail.com>
2021-10-20 23:14:46 -0300

    boolean type

---
+++ modulo-02/boolean/boolean.ts
@@ -0,0 +1,15 @@
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+
+// exempo 02
+let concluido: boolean = false;
+
+if (!concluido) {
+  console.log('Tarefa foi concluida com sucesso')
+} else {
+  console.log('Tarefa pendente')
+}
