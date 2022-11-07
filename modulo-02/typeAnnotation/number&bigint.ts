025e5a0d24767d53b32ac5960e99fe36ea746922
Italo Canturil <italo.cantur@gmail.com>
2022-11-06 23:29:21 -0300

    number e bigint

---
+++ modulo-02/typeAnnotation/number&bigint.ts
@@ -0,0 +1,9 @@
+let num1: number = 123; // number
+let num2: number = 0x37CF; // Hexadecimal
+let num3: number = 0o377; // octal
+let num4:number = 0b101110; // binary
+
+let big1: bigint = 90000223393892903n; // number
+let big2: bigint = 0b110000000000000000000000000000000000000000000000000011n; // binary
+let big3: bigint = 0x200000000000003n; // Hex
+let big4: bigint = 0o00000000000000000003n; // octal
