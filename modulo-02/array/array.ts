8694686779675f8b781da953088178bc39d20200
Italo Canturil <italo.cantur@gmail.com>
2022-12-14 22:18:10 -0300

    tipo array e string

---
+++ modulo-02/array/array.ts
@@ -0,0 +1,14 @@
+let frutas:string[] = ['uva', 'maca', 'banana']
+
+// Array Object
+let frutas1: Array<string> = ['banana', 'abacaxi']
+
+// adicionando mais strings com 'push'
+
+let idiomas: Array<string> = ['PT-br', 'Inglês', 'Espanhol']
+idiomas.push('Mandarim');
+
+// array com spread operator
+
+let num: Array<number> = [0, 1, 2, 3, 4, 5];
+num = [...num, 6, 7, 8, 9, 10]
