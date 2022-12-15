d1c060617463358db81e96efc4b9b2eb39962357
Italo Canturil <italo.cantur@gmail.com>
2022-12-14 22:14:54 -0300

    tipo array e string

---
+++ modulo-02/array/array.ts
@@ -0,0 +1,9 @@
+let frutas:string[] = ['uva', 'maca', 'banana']
+
+// Array Object
+let frutas1: Array<string> = ['banana', 'abacaxi']
+
+// adicionando mais strings com 'push'
+
+let idiomas: Array<string> = ['PT-br', 'Inglês', 'Espanhol']
+idiomas.push('Mandarim');
