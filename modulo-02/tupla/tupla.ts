893b69bdc9123b655465c669ccfd90149056a205
Italo Canturil <italo.cantur@gmail.com>
2022-12-15 22:34:32 -0300

    inicio de tupla

---
+++ modulo-02/tupla/tupla.ts
@@ -0,0 +1,7 @@
+// As tuplas nos permite armazenar campos com tipos diferentes
+// pode ser considerado um array com número fixo de elementos
+// pode ser passadas como parâmetros
+
+let pessoa: [string, string, number];
+
+pessoa = ['Italo', 'Front-End', 2001];
