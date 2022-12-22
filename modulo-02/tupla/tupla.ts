3e5eaca09387c8d0cd89c9963fdd17f13ff5fd85
Italo Canturil <italo.cantur@gmail.com>
2022-12-21 22:12:06 -0300

    exemplos de uso com tupla

--- modulo-02/tupla/tupla.ts
+++ modulo-02/tupla/tupla.ts
@@ -5,3 +5,32 @@
 let pessoa: [string, string, number];
 
 pessoa = ['Italo', 'Front-End', 2001];
+
+// Outra forma de usar Tuplas em Typescript
+
+let pessoa1: [nome: string, posicao:string, idade:number] = ['Italo', 'Front-End', 2001];
+
+// spread operator
+let listaFrutas: [string, ...string[]] = ['🍇', '🍍', '🥭', '🍉', '🍈'];
+console.log(...listaFrutas)
+
+// Lista Heterogênea de Tupla
+
+let listaFrutas2: [number, boolean, ...string[]] = [3, true, ...listaFrutas];
+
+
+// Tuplas com funções
+
+function userForm(username: string[], nascimento: string[], idade: number[]) {
+  return [...username, ...nascimento, ...idade]
+}
+
+// Tuplas com label e spread operator numa função
+
+type Nome = 
+  | [primeiroNome: string, sobrenome: string]
+  | [primeiroNome: string, nomeDoMeio: string, sobrenome: string]
+
+function criarPessoa(...nome: Nome) {
+  return [...nome]
+}
