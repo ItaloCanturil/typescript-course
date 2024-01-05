cfb4dc41410d2278a3ec5a227722afc1e4ecd625
Italo Canturil <italo.cantur@gmail.com>
2024-01-04 23:09:48 -0300

    feat: `tipo unknown`

---
+++ modulo-02/unknown/unknown.ts
@@ -0,0 +1,15 @@
+/* O tipo unknown é usado quando não sabemos qual tipo definir]
+
+  Qual a diferença dele para o tipo any?
+  - o tipo unknown vai evitar problemas pois o typescript vai fazer uma verificação de tipo antes de rodar o código, diferente do que acontece com o tipo any que não faz essa verificação e pode causar problemas mais pra frente
+
+  * Todo os tipos podem ser atribuidos ao tipo unknown
+
+  exemplos:
+*/
+
+let valorVariavel: unknown;
+
+valorVariavel = 123;
+valorVariavel = 'KOEEEEE';
+valorVariavel = []
